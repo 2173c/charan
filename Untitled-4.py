44fc096a1ea0dbd51dc2d068bd9fc61404f95292
2173c <charanpatil2173@gmail.com>
2026-02-03 15:01:52 +0530

    git init

---
+++ Untitled-4.py
@@ -0,0 +1,11 @@
+item="laptop"
+quantity=2
+price =499.99
+in_stock=True
+
+print("Item :",item,
+      "Qty :",quantity,
+      "price:",price,
+      "availability:",in_stock)
+ttl_cost=quantity * price
+print ("total cost:",ttl_cost)
